53128d45f24fa42a6a48ad2871db91992dc8bc02
Recsoft Popov <36furious@gmail.com>
2019-11-15 17:38:01 +0300

    Dog exelant

--- src/main/java/com/vsu/data/wave/Func.java
+++
@@ -1,7 +0,0 @@
-package com.vsu.data.wave;
-
-import org.apache.commons.math3.analysis.UnivariateFunction;
-import org.apache.commons.math3.optim.nonlinear.scalar.ObjectiveFunction;
-
-public interface Func extends UnivariateFunction {
-}
--- src/main/java/com/vsu/data/wave/Wave.java
+++
@@ -1,5 +0,0 @@
-package com.vsu.data.wave;
-
-public interface Wave {
-    double funcWeve(double x, int i, int j);
-}
